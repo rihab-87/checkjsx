93a253722ac1ac434fb2ba2cb02d4f29da97438d
rihab <rihab.ed@gmail.com>
2021-03-23 13:34:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
+import jsxx from "./jsxx.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container style={{ border: "solid 1px black", maxWidth: "80vw" }} className="App ">
+      <Row className="row">
+        <h1 className="title red font-weight-bolder">React Js</h1> <br />
+      </Row>
+      <Row>
+        <Col>
+          <img src={jsxx} alt="imageInSrc" className="img-fluid" />
+        </Col>
+        <Col>
+          <img src="react.png" alt="imageInPublic" className="img-fluid" />
+        </Col>
+      </Row>
+
+      <Row className="row">
+        <h2 className="title red">How to start with React and Jsx:</h2>
+      </Row>
+      <Row className="row">
+        <video  id="idvideo" style={{width:"320" , height:"240"}} controls >
+          <source src="../myVideo.mp4" type="video/mp4" />
+        </video>
+      </Row>
+    </Container>
   );
 }
 
